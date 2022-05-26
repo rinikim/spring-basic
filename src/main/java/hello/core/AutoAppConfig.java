caf45220bb63c48c8dfa13b3f21cf5a05d24743d
rinikim <tytntu@naver.com>
2022-05-26 09:47:26 +0900

    6. 컴포넌트 스캔 - 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
         // 보통 실무에서는 Configuration 를 제외하지 않는다.
         // AppConfig 를 제외하기 위해서, @Configuration 안에는 @Component 가 있기 때문에 ComponentScan 을 사용하면 scan 이 된다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
